0e98e68b80ab94e911df5456703fd27c77ec3005
Th. Androutsos <a8anassis@gmail.com>
2024-09-04 21:03:05 +0300

    SecUtil implemented and user dao created

---
+++ src/main/java/gr/aueb/cf/schoolapp/security/SecUtil.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.schoolapp.security;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class SecUtil {
+
+    private SecUtil() {}
+
+    public static String hashPassword(String inputPasswd) {
+        int workload = 12;
+        String salt = BCrypt.gensalt(workload);
+        return BCrypt.hashpw(inputPasswd,salt);
+    }
+
+    public static boolean isPasswordValid(String inputPasswd, String storedHashedPasswd) {
+        return BCrypt.checkpw(inputPasswd, storedHashedPasswd);
+    }
+}
