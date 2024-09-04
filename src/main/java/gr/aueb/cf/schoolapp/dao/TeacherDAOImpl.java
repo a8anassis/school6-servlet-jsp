56d4685ce0ec9e6d78774ebe1ca26ddf8852edba
Th. Androutsos <a8anassis@gmail.com>
2024-09-04 20:36:59 +0300

    dao completed

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -6,7 +6,9 @@ import gr.aueb.cf.schoolapp.service.util.DBUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeacherDAOImpl implements ITeacherDAO {
@@ -80,11 +82,52 @@ public class TeacherDAOImpl implements ITeacherDAO {
 
     @Override
     public Teacher getById(Integer id) throws TeacherDAOException {
-        return null;
+        String sql = "SELECT * FROM teachers WHERE id = ?";
+        Teacher teacher = null;
+        ResultSet rs;
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                teacher = new Teacher(rs.getInt("id"), rs.getString("firstname"), rs.getString("lastname"));
+            }
+            // Logging
+            return teacher;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in get by id with id:  " + id);
+        }
     }
 
     @Override
     public List<Teacher> getFilteredTeachers(String firstname, String lastname) throws TeacherDAOException {
-        return null;
+        String sql = "SELECT * FROM teachers WHERE firstname LIKE ? AND lastname LIKE ?";
+        List<Teacher> teachers = new ArrayList<>(); // isEmpty == true
+        ResultSet rs;
+        Teacher teacher;
+
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, firstname + "%");
+            ps.setString(2, lastname + "%");
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                teacher = new Teacher(rs.getInt("id"), rs.getString("firstname"), rs.getString("lastname"));
+                teachers.add(teacher);
+            }
+            // Logging
+            return teachers;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // logging
+            throw new TeacherDAOException("SQL error in filtered get");
+        }
     }
 }
