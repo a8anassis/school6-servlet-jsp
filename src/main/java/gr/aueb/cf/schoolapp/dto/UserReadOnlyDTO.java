01efae3a1d54ae10c2d105080c655caedbf25ced
Th. Androutsos <a8anassis@gmail.com>
2024-09-06 20:48:34 +0300

    in user readonly dto confirmed password not needed any more

--- src/main/java/gr/aueb/cf/schoolapp/dto/UserReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/UserReadOnlyDTO.java
@@ -1,20 +1,10 @@
 package gr.aueb.cf.schoolapp.dto;
 
 public class UserReadOnlyDTO extends BaseUserDTO {
-    private Integer id;
 
     public UserReadOnlyDTO() {}
 
-    public UserReadOnlyDTO(Integer id, String username, String password) {
-        super(username, password);
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public UserReadOnlyDTO(String username, String password, String confirmedPassword) {
+        super(username, password, confirmedPassword);
     }
 }
